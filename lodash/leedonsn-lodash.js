4f0d7fd6966d7601b9b1cd15d0ca7682451e302b
leedonsn <123807472@qq.com>
2019-01-28 14:53:54 +0800

    correct difference and correct chunk

--- lodash/leedonsn-lodash.js
+++ lodash/leedonsn-lodash.js
@@ -2,7 +2,7 @@ var leedonsn = function(){
 
     function chunk(ary, size = 1) {
         var result = []
-        for(var i = 0; i<ary.length; i++) {
+        for(var i = 0; i<ary.length; i+=size) {
             result.push(ary.slice(i, i + size))
         }
         return result
@@ -16,10 +16,14 @@ var leedonsn = function(){
         }
         return result
     }
-    function difference(array, [values]) {
+    function difference(array,...args) {
         var result = []
+        var arguments = []
+        for(i=0;i<args.length;i++){
+            arguments = arguments.concat(args[i])
+        }
         for(var i=0;i<array.length;i++){
-            if(values.indexOf(array[i])==-1){
+            if(arguments.indexOf(array[i])==-1){
                 result.push(array[i])
             }
         }
